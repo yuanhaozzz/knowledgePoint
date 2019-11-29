e0157e516bd57039b92e348ff12f7a40ff469386
yuanhao <yuanhao1@beiwaiguoji.com>
2019-11-29 18:00:05 +0800

    提交

--- template/react/client/src/views/home/index.js
+++ template/react/client/src/views/home/index.js
@@ -14,7 +14,7 @@ class index extends Component {
             console.log(res, '----------------')
             return res
         })
-        
+
     }
 
     constructor(props) {
@@ -49,7 +49,7 @@ class index extends Component {
 }
 
 let mapStateToProps = state => {
-    let { test,test1 } = state.test
+    let { test, test1 } = state.test
     return {
         test,
         test1
@@ -60,3 +60,67 @@ export default connect(
     mapStateToProps,
     { setTest, setTest1 }
 )(index);
+
+
+// import React, { Component } from 'react'
+
+// import { connect } from 'react-redux'
+// import { getQAType, getQaQuestionList } from '../../../../store/actions/qa'
+// import './home.less'
+// import api from '../../../../api/apiHost'
+
+
+// class Home extends Component {
+//     static getInintalProps (store, api, params) {
+//         return store.dispatch(getQAType(params, api)).then(res => {
+//             return store.dispatch(getQaQuestionList(params, api)).then(res => {
+//                 return res
+//             })
+//         })
+//     }
+
+//     constructor(props) {
+//         super(props);
+//         this.state = {};
+//     }
+
+//     componentDidMount () {
+//         let { homeType, homeQuestionList } = this.props
+//         console.log(homeType, 'homeType')
+//         console.log(homeQuestionList, 'homeQuestionList')
+//         let params = {
+//             action: 'getQAType',
+//             displayTerminal: 2
+//         }
+//         api.sendBaseApi(params).then(res => {
+//             console.log(res)
+//         })
+//     }
+
+//     render () {
+//         return (
+//             <div className='qa-wrapper'>
+//                 {/* <header>
+//                     {this.renderSearch()}
+//                 </header>
+//                 <section>
+//                     {this.renderTabContent()}
+//                 </section> */}
+//             </div>
+//         );
+//     }
+// }
+
+// let mapStateToProps = state => {
+//     let { homeType, homeQuestionList } = state.qa
+//     console.log(state, '-=-=-=-=-=-=-=')
+//     return {
+//         homeType,
+//         homeQuestionList
+//     }
+// }
+
+// export default connect(
+//     mapStateToProps,
+//     { getQAType, getQaQuestionList }
+// )(Home);
